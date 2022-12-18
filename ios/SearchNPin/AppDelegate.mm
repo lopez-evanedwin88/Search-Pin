8cbf25cf0843e94b59a30410812f169738c05591
Evan Edwin Lopez <elopez@iripple.com>
2022-12-18 19:09:13 +0800

    Integrate google-maps

--- ios/SearchNPin/AppDelegate.mm
+++ ios/SearchNPin/AppDelegate.mm
@@ -15,6 +15,7 @@
 #import <ReactCommon/RCTTurboModuleManager.h>
 
 #import <react/config/ReactNativeConfig.h>
+#import <GoogleMaps/GoogleMaps.h>
 
 static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 
@@ -32,7 +33,7 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   RCTAppSetupPrepareApp(application);
-
+  [GMSServices provideAPIKey:@"AIzaSyBBc_Avcozlpy_wQ9QywmVAJWYH81973NM"];
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
 
 #if RCT_NEW_ARCH_ENABLED
